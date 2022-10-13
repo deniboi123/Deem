9932068bdf36bd798eeb2d99bb73f61cf3c8c653
Denny <58114191+deniboi123@users.noreply.github.com>
2022-10-13 21:22:57 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+echo "
+<div align='center'>
+
+Hello, World! Start off by going into your <a href='admin/index.php'>Admin panel</a> The defualt password is:
+<br>
+<br>
+<b>Username: Admin</b>
+<br>
+<br>
+<b>Password: ThisPasswordIsInsecure</b>
+<br>
+<br>
+<br>
+<button>Visit Admin Panel</button>
+<br>
+You will be prompted to change this.
+</div>";
+
+?>
